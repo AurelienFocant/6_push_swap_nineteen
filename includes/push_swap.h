9b4ee5c5ad39c0230a80208c32ebf5cc731846af
Aurelien <afocant@student.s19.be>
2024-07-04 00:22:32 +0200

    debugged missing arguments in error checking functions, added prototypes in header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -2,5 +2,10 @@
 # define PUSH_SWAP_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
+/*----------------  errors_utils.c  ---------------*/
+void    ft_error_and_exit(char *msg, int nb_error);
+void    ft_check_arg_errors(int argc, char **argv);
 
 #endif
--- src/errors_utils.c
+++ src/errors_utils.c
@@ -6,12 +6,13 @@ void	ft_error_and_exit(char *msg, int nb_error)
 	exit(nb_error);
 }
 
-void	ft_check_arg_errors(int argc, char **)
+void	ft_check_arg_errors(int argc, char **argv)
 {
 	if (argc < 2)
 		ft_error_and_exit("Error", 1);
 
 	if (argc == 2)
+		(void) argv;
 		// if just one digit, it is sorted already
 		// otherwise if a string
 		// split the string
