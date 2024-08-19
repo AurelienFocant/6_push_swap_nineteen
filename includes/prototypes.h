f750d0bcc6516ca37da0361b65854b38deef539e
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-08-19 18:57:20 +0100

    created prototype file

--- includes/prototypes.h
+++ includes/prototypes.h
@@ -1,33 +1,10 @@
+#include "push_swap.h"
 
-/*----------------  the_pseudocode.c  ---------------*/
-
-/*----------------  errors_utils.c  ---------------*/
-void	ft_print_error(char *str);
-void	ft_error_and_exit(char *msg, int nb_error);
-int		ft_is_str_nbr(char *str);
-void	ft_check_non_integers(char **argv);
-void	ft_check_duplicates(char **argv, size_t size);
-void	ft_check_arg_errors(int argc, char **argv);
-
-/*----------------  list_init.c  ---------------*/
-void	ft_print_node_data(t_node *node);
-void	ft_init_stack(char **argv, t_node **stack_a);
-void	ft_create_node(int data, t_node **stack_a);
-void	ft_free_linkedlist(t_node *lst);
-void	ft_free_lists(t_node *stack_a, t_node *stack_b);
-
-/*----------------  print_list.c  ---------------*/
-void	ft_print_list(t_node *stack);
-void	ft_print_stack(t_node *stack, char *name);
-
-/*----------------  search_list.c  ---------------*/
-t_node	*ft_get_last_node(t_node **stack);
-t_node	*ft_get_penult_node(t_node **stack, t_node **last_node);
-
-/*----------------  main.c  ---------------*/
-int	print(void);
-void	ft_test_ops(t_node *stack_a, t_node *stack_b);
-int main(int argc, char **argv);
+/*----------------  rotate.c  ---------------*/
+void	ft_rotate(t_node **stack);
+void	ra(t_node **stack);
+void	rb(t_node **stack);
+void	rr(t_node **stack_a, t_node **stack_b);
 
 /*----------------  push.c  ---------------*/
 void	ft_push(t_node **stack_from, t_node **stack_to);
@@ -40,16 +17,35 @@ void	rra(t_node **stack);
 void	rrb(t_node **stack);
 void	rrr(t_node **stack_a, t_node **stack_b);
 
-/*----------------  rotate.c  ---------------*/
-int	print(void);
-void	ft_rotate(t_node **stack);
-void	ra(t_node **stack);
-void	rb(t_node **stack);
-void	rr(t_node **stack_a, t_node **stack_b);
-
 /*----------------  swap.c  ---------------*/
 void	ft_swap(t_node **stack);
 void	sa(t_node **stack_a);
 void	sb(t_node **stack_b);
 void	ss(t_node **stack_a, t_node **stack_b);
 
+/*----------------  main.c  ---------------*/
+void	ft_test_ops(t_node *stack_a, t_node *stack_b);
+int main(int argc, char **argv);
+
+/*----------------  errors_utils.c  ---------------*/
+void	ft_print_error(char *str);
+void	ft_error_and_exit(char *msg, int nb_error);
+int		ft_is_str_nbr(char *str);
+void	ft_check_non_integers(char **argv);
+void	ft_check_duplicates(char **argv, size_t size);
+void	ft_check_arg_errors(int argc, char **argv);
+
+/*----------------  print_list.c  ---------------*/
+void	ft_print_list(t_node *stack);
+void	ft_print_stack(t_node *stack, char *name);
+
+/*----------------  list_init.c  ---------------*/
+void	ft_print_node_data(t_node *node);
+void	ft_init_stack(char **argv, t_node **stack_a);
+void	ft_create_node(int data, t_node **stack_a);
+void	ft_free_linkedlist(t_node *lst);
+void	ft_free_lists(t_node *stack_a, t_node *stack_b);
+
+/*----------------  search_list.c  ---------------*/
+t_node	*ft_get_last_node(t_node **stack);
+t_node	*ft_get_penult_node(t_node **stack, t_node **last_node);
